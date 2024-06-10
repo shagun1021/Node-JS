2b7efa439bbe0fd0f99cf9b394801d33fb95b80b
Shagun <154508782+shagun1021@users.noreply.github.com>
2024-06-11 04:33:20 +0530

    Create module.js

---
+++ module.js
@@ -0,0 +1,16 @@
+// const imp=require("./calculator");
+
+// console.log(imp(2,3));
+// console.log(imp.sub(20,10));
+
+
+
+// const imp=require("./calculator");
+const {add,sub}=require("./calculator"); // destructuring
+
+// console.log(imp.add(1,2));
+// console.log(imp.sub(2,1));
+
+console.log(add(1,2)); // if destructuring
+console.log(sub(2,1)); // if destructuring
+// console.log(imp.sub(20,10));
