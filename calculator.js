6ce3fb4525ac16239b422cf1658f767896a71451
Shagun <154508782+shagun1021@users.noreply.github.com>
2024-06-11 04:34:02 +0530

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,16 @@
+// function add(x,y){
+//   return x+y;
+// }
+// function sub(x,y){
+//   return x-y;
+// }
+
+// module.exports={add,sub};
+
+
+
+// :::::::::: ANOTHER WAY OF EXPORTING :::::::::::::
+
+
+exports.add=(x,y)=>x+y;
+exports.sub=(x,y)=>x-y;
